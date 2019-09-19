7c80d9a71cce0ed3216c0afc60b7b55e2fe6fe6b
Esther Park <estherspark91@gmail.com>
2019-09-18 20:03:45 -0400

    added SingleTrack component and now rendering Top 10 Tracks

---
+++ src/components/tracks/SingleTrack.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const SingleTrack = props => {
+  const { track } = props;
+
+  // console.log(track.album_coverart_100x100); //undefined?
+  return (
+    <div className="col-md-6">
+      <div className="card mb-4 shadow-sm">
+        <div className="card-body">
+          <h5>{track.artist_name}</h5>
+          <p className="card-text">
+            <strong>
+              <i className="fas fa-play"></i> Track
+            </strong>
+            : {track.track_name}
+            <br />
+            <strong>
+              <i className="fas fa-compact-disc"></i> Album
+            </strong>
+            : {track.album_name}
+          </p>
+          {/* Button */}
+          {/* NOT using a tag - in react router using Link */}
+          <Link
+            to={`lyrics/track/${track.trac_id}`}
+            className="btn btn-dark btn-block"
+          >
+            <i className="fas fa-chevron-right"></i> View Lyrics
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleTrack;
--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
 import { Consumer } from '../../context';
 import Spinner from '../layout/Spinner';
+import SingleTrack from './SingleTrack';
 
-// check if the tracks are there and is the array empty or not
-// if array is empty -> spinner
+// check if the tracks are there and is the array empty or not, if array is empty -> spinner
 class Tracks extends Component {
   render() {
     return (
       <Consumer>
         {/* currently, the value = the entire state */}
         {value => {
-          const { track_list } = value;
+          const { track_list, heading } = value;
 
           if (track_list === undefined || !track_list.length) {
             return <Spinner />;
           } else {
-            return <h1>Tracks loaded</h1>;
+            return (
+              <React.Fragment>
+                <h3 className="text-center mb-4">{heading}</h3>
+                <div className="row">
+                  {track_list.map(item => (
+                    <SingleTrack key={item.track.track_id} track={item.track} />
+                  ))}
+                </div>
+              </React.Fragment>
+            );
           }
         }}
       </Consumer>
